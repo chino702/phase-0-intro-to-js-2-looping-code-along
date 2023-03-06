9f0ead76526576e336cd1513f30cf269c772ef4c
chino702 <chinox3702@gmail.com>
2023-03-06 15:36:10 -0800

    this is a loop lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+function writeCards(names, value){
+  let messageNames = [];
+  for (let i = 0; i < names.length; i++) {
+    messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+
+  }
+  return messageNames;
+};
+
+writeCards(["Guadalupe","Ollie", "Aki"], "Surprise");
+
+function countDown(){
+  for(
+    let i = 10;
+    i > -1;
+    i--
+
+  ){
+    console.log(i);
+  }
+}
+
+countDown();
